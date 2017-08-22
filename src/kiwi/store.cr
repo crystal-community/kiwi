d2b7e1ee9b3ca4ef4916f02eb3ca1c98b267f05b
Vitalii Elenhaupt <velenhaupt@gmail.com>
2017-08-22 20:12:20 +0300

    Format sources

--- src/kiwi/store.cr
+++ src/kiwi/store.cr
@@ -1,8 +1,8 @@
 module Kiwi
   abstract class Store
-    abstract def get(key : String) : String|Nil
+    abstract def get(key : String) : String | Nil
     abstract def set(key : String, val : String) : String
-    abstract def delete(key : String) : String|Nil
+    abstract def delete(key : String) : String | Nil
     abstract def clear : Store
 
     def []=(*args)
